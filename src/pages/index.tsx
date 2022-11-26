736ec18c09e4d25ec5eca5f4d74ba205b29b31f3
KaungMyatLin <kaungmyatlin.worklife@gmail.com>
2022-11-26 10:09:53 +0630

    fix tailwind not work + u

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,25 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container} >
       <Head>
-        <title>Nextjs with tailwind theme</title>
+        <title>Nextjs with darkmode by tailwind</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main >
-        <h1>
+      <main className="
+      flex flex-col justify-center items-center">
+        <h1 className="text-2xl">
           NEXT JS Template For Toggling themes using Tailwind CSS. This is a
           template repository that you can use for creating a new repo based on
           it.
         </h1>
         <div>
-          <h1>
+          <h1 className="text-2xl mt-10">
             <a href="https://github.com/hossein13m/nextjs-tailwind-dark-theme"
-              target="blank" >
-                <button type="button">
-                  <span>GitHub Repository</span>
+            target="_blank"
+            >
+                <button className="px-1 py-1 bg-zinc-800 text-gray-100 dark:bg-zinc-100 dark:text-gray-800 rounded
+                " type="button">
+                  <span className="mr-2">GitHub Repository</span>
                   <Image
                     src="/github.png"
                     alt="Github Logo"
@@ -30,6 +33,22 @@ const Home: NextPage = () => {
                     height={20}
                     />
                 </button>
+            </a>
+          </h1>
+          <h1 className="text-2xl mt-10">
+            <a href="https://javascript.plainenglish.io/how-to-implement-dark-light-themes-in-a-next-js-app-using-context-hook-tailwindcss-336558dd4579"
+            target="_blank"
+            >
+              <button className="px-1 py-1 mx-2 bg-zinc-800 text-gray-100 dark:bg-zinc-100 dark:text-gray-800 rounded
+              " type="button">
+                <span className="mr-2">Medium article</span>
+                <Image className=""
+                src="/medium.png"
+                alt=""
+                width={20}
+                height={20}
+                />
+              </button>
             </a>
           </h1>
         </div>
